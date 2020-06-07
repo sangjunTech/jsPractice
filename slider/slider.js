afd098ea1e4bc4e60478d991c774c304dfa6aab8
sangjun <juntech0421@github.com>
2020-06-07 18:59:30 +0900

    add slider code

---
+++ slider/slider.js
@@ -0,0 +1,19 @@
+const SHOWING_CLASS = 'showing';
+const firstSlide = document.querySelector('.slider:first-child');
+function slide() {
+    const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+    if(currentSlide){
+        currentSlide.classList.remove(SHOWING_CLASS);
+        const nextSlide = currentSlide.nextElementSibling;
+        if(nextSlide){
+            nextSlide.classList.add(SHOWING_CLASS);
+        } else {
+            firstSlide.classList.add(SHOWING_CLASS);
+        }
+    }else {
+        firstSlide.classList.add(SHOWING_CLASS);
+    }
+}
+
+slide();
+setInterval(slide, 3000);
